0633411fa837d762114d4f658894c5e86ad981e3
Raghav Shrestha <raghavshrestha528@gmail.com>
2026-06-23 21:37:52 +0545

    Clean up repo to focus on OCR and ML pipeline comparison work

--- MediSafe/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from MediSafe.models import Users
-from MediSafe.models import UserProfile
-from MediSafe.models import UserMedications
-
-admin.site.register(Users)
-admin.site.register(UserProfile)
-admin.site.register(UserMedications)
-# Register your models here.
--- MediSafe/static/MediSafe/js/resetAccount.js
+++
@@ -1,5 +0,0 @@
-let go_back_div = document.getElementById("go_back_div");
-
-go_back_div.addEventListener("click", (e) => {
-  window.location.href = "/login/";
-});
